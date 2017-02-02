bd66d5c90ada4dd351949c0f8911f2f1af8fd179
Alessandro Resta <alessandro.resta@gmail.com>
2017-02-02 00:52:20 +0000

    Code quality and unit tests
     - move logic from main.go to parser.go
     - increment unit test coverage
     - update binary
     - add govendor with dependencies
     - add spinner for loading time

---
+++ src/parser.go
@@ -0,0 +1,157 @@
+package parser
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/briandowns/spinner"
+)
+
+// Review struct for JSON values
+type Review struct {
+	Votes struct {
+		Funny  int `json:"funny"`
+		Useful int `json:"useful"`
+		Cool   int `json:"cool"`
+	} `json:"votes"`
+	UserID     string `json:"user_id"`
+	ReviewID   string `json:"review_id"`
+	Stars      int    `json:"stars"`
+	Date       string `json:"date"`
+	Text       string `json:"text"`
+	Type       string `json:"type"`
+	BusinessID string `json:"business_id"`
+}
+
+const (
+	inputFile          = "data/yelp_academic_dataset_review.json"
+	outputFile         = "data/yelp_academic_dataset_review.tsv"
+	delimiter          = "{\"votes\":"
+	errorMsgUnmarshal  = "failed to unmarshal json"
+	errorMsgWriteOut   = "failed to write output"
+	errorMsgReadFile   = "failed to open input file"
+	loadingMsgProcess  = "Processing data "
+	loadingMsgWrite    = "Writing output "
+	loadingMsgComplete = "Ouput file: yelp_academic_dataset_review.tsv\n"
+)
+
+var loading = spinner.New(spinner.CharSets[24], 100*time.Millisecond)
+
+// Run triggers data processing
+func Run() {
+	loading.Prefix = loadingMsgProcess
+	loading.Start()
+
+	var review []*Review
+
+	lineCh := make(chan string, 100)
+
+	go readFile(inputFile, lineCh)
+
+	for line := range lineCh {
+		index := getIndexPosition(line, delimiter)
+		if index != -1 {
+			r, err := unmarshal(line[index:])
+			if err != nil {
+				log.Fatalf("%s => %s", errorMsgUnmarshal, err)
+			}
+			review = append(review, r)
+		}
+	}
+	loading.Stop()
+	loading.Prefix = loadingMsgWrite
+	loading.FinalMSG = loadingMsgComplete
+	loading.Start()
+
+	if err := writeOut(review, outputFile); err != nil {
+		log.Fatalf("%s => %s", errorMsgWriteOut, err)
+	}
+	loading.Stop()
+}
+
+// Read dataset and send lines through string channel
+func readFile(path string, lineCh chan string) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("%s => %s", errorMsgWriteOut, err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lineCh <- scanner.Text()
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	close(lineCh)
+}
+
+// Parse JSON to Review struct
+func unmarshal(jsonStr string) (*Review, error) {
+	var r Review
+	if err := json.Unmarshal([]byte(jsonStr), &r); err != nil {
+		return &Review{}, err
+	}
+	return &r, nil
+}
+
+// Write stars and review text on a CSV file
+func writeOut(review []*Review, outputFile string) error {
+	file, err := os.Create(outputFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+	_, err = w.WriteString("star\treview\n")
+	if err != nil {
+		return err
+	}
+
+	w.Flush()
+
+	for _, r := range review {
+		text := removeNewlines(r.Text)
+		text = removeTabs(text)
+		star := normaliseStars(r.Stars)
+		row := fmt.Sprintf("%d\t%s\n", star, strings.ToLower(text))
+		_, err := w.WriteString(row)
+		if err != nil {
+			return err
+		}
+		w.Flush()
+	}
+
+	return nil
+}
+
+// Remove newlines from review text
+func removeNewlines(text string) string {
+	return strings.Replace(text, "\n", " ", -1)
+}
+
+// Remove tabs from review text
+func removeTabs(text string) string {
+	return strings.Replace(text, "\t", " ", -1)
+}
+
+// Normalise stars for binary values
+func normaliseStars(star int) int {
+	if star > 3 {
+		return 1
+	}
+	return 0
+}
+
+// Get index position of given substring
+func getIndexPosition(str, substr string) int {
+	return strings.Index(str, substr)
+}
---
+++ src/parser_test.go
@@ -0,0 +1,133 @@
+package parser
+
+import (
+	"bufio"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+const testFile = "../test/input.json"
+
+func TestUnmarshal(t *testing.T) {
+	expectedStars := 4
+	expectedText := `Mr Hoagie is an institution. Walking in, it does seem like a throwback to 30 years ago, old fashioned menu board, booths out of the 70s, and a large selection of food. Their speciality is the Italian Hoagie, and it is voted the best in the area year after year. I usually order the burger, while the patties are obviously cooked from frozen, all of the other ingredients are very fresh. Overall, its a good alternative to Subway, which is down the road.`
+
+	buf, err := ioutil.ReadFile(testFile)
+	if err != nil {
+		t.Error(err)
+	}
+
+	file := string(buf)
+
+	r, err := unmarshal(file)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if r.Stars != 4 {
+		t.Errorf("Expecting %d stars. Got %d", expectedStars, r.Stars)
+	}
+
+	if r.Text != expectedText {
+		t.Errorf("Expecting text:\n%s\nGot:\n%s\n", expectedText, r.Text)
+	}
+}
+
+func TestWriteOut(t *testing.T) {
+	a := &Review{
+		Stars: 1,
+		Text:  "foo",
+	}
+	b := &Review{
+		Stars: 2,
+		Text:  "bar",
+	}
+	c := &Review{
+		Stars: 3,
+		Text:  "foobar",
+	}
+
+	review := []*Review{a, b, c}
+
+	testOutput := "../test/writeout.txt"
+	if err := writeOut(review, testOutput); err != nil {
+		t.Error(err)
+	}
+
+	file, err := os.Open(testOutput)
+	if err != nil {
+		t.Error(err)
+	}
+
+	expected := []string{"star	review", "0	foo", "0	bar", "0	foobar"}
+
+	scanner := bufio.NewScanner(file)
+	for i := 0; scanner.Scan(); i++ {
+		if scanner.Text() != expected[i] {
+			t.Errorf("Expecting: %s. Got: %s", expected[i], scanner.Text())
+		}
+	}
+}
+
+func TestRemoveNewLines(t *testing.T) {
+	input := "foo\nbar\n"
+	expected := "foo bar "
+	if observed := removeNewlines(input); observed != expected {
+		t.Errorf("Expected: %s. Got: %s", expected, observed)
+	}
+}
+
+func TestRemoveTabs(t *testing.T) {
+	input := "foo\tbar\t"
+	expected := "foo bar "
+	if observed := removeTabs(input); observed != expected {
+		t.Errorf("Expected: %s. Got: %s", expected, observed)
+	}
+}
+
+var getIndexPositionCases = []struct {
+	inputStr, inputSubstr string
+	expected              int
+}{
+	{"foobar", "bar", 3},
+	{"foobar", "fizz", -1},
+}
+
+func TestGetIndexPosition(t *testing.T) {
+	for _, test := range getIndexPositionCases {
+		if observed := getIndexPosition(test.inputStr, test.inputSubstr); observed != test.expected {
+			t.Errorf("For string %s and substring %s. Expected: %d. Got: %d",
+				test.inputStr, test.inputSubstr, test.expected, observed)
+		}
+	}
+}
+
+var normaliseStarsCases = []struct {
+	input, expected int
+}{
+	{4, 1},
+	{2, 0},
+}
+
+func TestNormaliseStars(t *testing.T) {
+	for _, test := range normaliseStarsCases {
+		if observed := normaliseStars(test.input); observed != test.expected {
+			t.Errorf("For star: %d. Expected: %d. Got: %d",
+				test.input, test.expected, observed)
+		}
+	}
+}
+
+func TestReadFile(t *testing.T) {
+	expected := `{"votes": {"funny": 0, "useful": 0, "cool": 0}, "user_id": "PUFPaY9KxDAcGqfsorJp3Q", "review_id": "Ya85v4eqdd6k9Od8HbQjyA", "stars": 4, "date": "2012-08-01", "text": "Mr Hoagie is an institution. Walking in, it does seem like a throwback to 30 years ago, old fashioned menu board, booths out of the 70s, and a large selection of food. Their speciality is the Italian Hoagie, and it is voted the best in the area year after year. I usually order the burger, while the patties are obviously cooked from frozen, all of the other ingredients are very fresh. Overall, its a good alternative to Subway, which is down the road.", "type": "review", "business_id": "5UmKMjUEUNdYWqANhGckJw"}`
+
+	lineCh := make(chan string)
+	go readFile(testFile, lineCh)
+
+	for line := range lineCh {
+		if line != expected {
+			t.Errorf("Expecting line:\n%s\nGot:\n%s\n", expected, line)
+		}
+	}
+}
